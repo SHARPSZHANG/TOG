4f618735ce3be5cad4e98d479f305890ef2a8744
sharpszhang <sharpszhang@qq.com>
2022-11-30 18:30:02 +0800

    【android】接口调整

--- server/RuoYi-Vue-master/ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysRegisterController.java
+++ server/RuoYi-Vue-master/ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysRegisterController.java
@@ -35,4 +35,15 @@ public class SysRegisterController extends BaseController
         String msg = registerService.register(user);
         return StringUtils.isEmpty(msg) ? success() : error(msg);
     }
+
+    @PostMapping("/toRegister")
+    public AjaxResult toRegister(@RequestBody RegisterBody user)
+    {
+        if (!("true".equals(configService.selectConfigByKey("sys.account.registerUser"))))
+        {
+            return error("当前系统没有开启注册功能！");
+        }
+        String msg = registerService.register(user);
+        return StringUtils.isEmpty(msg) ? AjaxResult.success(true) : error(msg);
+    }
 }
