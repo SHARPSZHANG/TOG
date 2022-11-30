d2628d95973411fbcc1345ea6bd329d5b302fac3
xuzili <775509414@qq.com>
2022-11-30 19:31:51 +0800

    1

---
+++ server/RuoYi-Vue-master/ruoyi-admin/src/main/java/com/ruoyi/web/controller/tool/UserUtils.java
@@ -0,0 +1,20 @@
+package com.ruoyi.web.controller.tool;
+
+import com.ruoyi.common.constant.HttpStatus;
+import com.ruoyi.common.core.domain.entity.SysUser;
+import com.ruoyi.common.core.domain.model.LoginUser;
+import com.ruoyi.common.exception.ServiceException;
+import com.ruoyi.web.controller.vo.SysUserVo;
+
+public class UserUtils {
+
+    /**
+     * 获取用户
+     **/
+    public static SysUserVo getLoginUser(SysUser user)
+    {
+        SysUserVo sysUserVo = new SysUserVo();
+//        sysUserVo.set
+        return sysUserVo;
+    }
+}
